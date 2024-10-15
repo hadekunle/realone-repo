6a7c04ae0977feca85fd34029695222e40400c98
hadekunle <shitoheeb@gmail.com>
2024-10-15 17:11:52 -0500

    add back sonarqube

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -31,6 +31,9 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 sudo usermod -aG docker jenkins
 sudo usermod -aG docker ubuntu
 sudo usermod -aG docker $USER
+# add on to fix permission issues 
+sudo systemctl restart docker
+sudo reboot
 curl -LO https://dl.k8s.io/release/v1.21.0/bin/linux/amd64/kubectl
 curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
